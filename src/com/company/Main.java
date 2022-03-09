bd1d3c0d678aa28bf398ad83544e6b35895f428c
Notnik_KG <tasnim0800@gmail.com>
2022-03-09 17:16:17 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Set<Integer> set1 = new HashSet<>();
+        set1.add(5);
+        set1.add(8);
+        set1.add(9);
+        System.out.println("Set first: " + set1);
+
+        Set<Integer> set2 = new HashSet<>();
+        set2.add(2);
+        set2.add(9);
+        set2.add(8);
+        System.out.println("Set second: " + set2);
+
+        symmetricDifference(set1,set2);
+    }
+        public static Set<Integer> symmetricDifference (Set<Integer> set1, Set<Integer> set2) {
+            Set<Integer> symmetricDiff = new HashSet<Integer>(set1);
+            symmetricDiff.addAll(set2);
+            Set<Integer> tmp = new HashSet<>(set1);
+            tmp.retainAll(set2);
+            symmetricDiff.removeAll(tmp);
+            System.out.println("Symmetric difference of two sets: "+symmetricDiff);
+              return symmetricDiff;
+        }
+    }
+
